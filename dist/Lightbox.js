29e0eb8c314d9ff2bd077dd25d75a1f0500801a3
Enzey <enzey@enzey.net>
2014-12-16 23:07:54 -0600

    Lightbox Example v2

---
+++ dist/Lightbox.js
@@ -0,0 +1,126 @@
+(function (angular) {
+    "use strict";
+
+	var module = angular.module('net.enzey.lightbox', ['net.enzey.services']);
+
+	module.directive('nzLightbox', ['$compile', '$document', function($compile, $document) {
+		return {
+			priority: 1000,
+			terminal: true,
+			replace: true,
+			template: function(element, attrs) {
+				var wrapper = angular.element('<div data-nz-lightbox-logic></div>');
+				var mask = angular.element('<div class="screenMask"></div>');
+				if (!angular.isDefined(attrs.noMask)) {
+					wrapper.append(mask);
+				}
+
+				var lightbox = angular.element('<div class="lightbox"></div>');
+				var form = angular.element('<form name="lightboxForm"></form>');
+				form.attr('nz-include-transclude', attrs[this.name]);
+				form.append(element.children());
+				lightbox.append(form);
+				wrapper.append(lightbox);
+
+				return wrapper[0].outerHTML;
+			},
+			compile: function($element, $attrs) {
+
+				return {
+					pre: function(scope, element, attrs) {
+						var cElem;
+						scope.$on(attrs.showEvent, function() {
+							if (!cElem || cElem.parent().length === 0) {
+								cElem = element.clone();
+								angular.element($document[0].body).append(cElem);
+								$compile(cElem, null, 1000)(scope);
+							}
+						});
+						element.remove();
+					},
+					post: function(scope, element, attrs) {
+					}
+				}
+			}
+		}
+	}]);
+
+	module.directive('nzLightboxLogic', ['nzService', function(nzService) {
+		return {
+			// Each Lightbox needs its own scope.
+			//   If dialogs are nested (popups in popups) then the lower level lightbox can squelch the event
+			//   so the top level lightbox does not propigate all thr way through.
+			scope: true,
+			require: 'nzLightboxLogic',
+			controller: ['$scope', '$element', function($scope, $element) {
+				$scope.$on('lightboxAccept', function($event) {
+					console.log('squelching: ' + 'lightboxAccept');
+				});
+				$scope.$on('lightboxReject', function($event) {
+					console.log('squelching: ' + 'lightboxReject');
+				});
+
+				var ctrl;
+
+				ctrl = {
+					acceptForm: function() {
+						$scope.$emit('lightboxAccept', $scope);
+						$element.remove();
+					},
+					rejectForm: function() {
+						$scope.$emit('lightboxReject');
+						$element.remove();
+					}
+				};
+
+				return ctrl;
+			}],
+			link: function(scope, element, attrs, thisCtrl) {
+				
+				if (!angular.isDefined(attrs.modal)) {
+					var lightboxElem = element[0].querySelector('.lightbox');
+					lightboxElem = angular.element(lightboxElem);
+					nzService.registerClickAwayAction(function(event) {
+						thisCtrl.rejectForm();
+						element.remove();
+					}, lightboxElem);
+				}
+
+			}
+		}
+	}]);
+
+	module.directive('lightboxAcceptAction', ['$compile', function($compile) {
+		return {
+			priority: 500,
+			terminal: true,
+			require: '^nzLightboxLogic',
+			compile: function($element, $attrs) {
+				$element.attr('data-ng-disabled', 'lightboxForm.$invalid');
+				return function(scope, element, attrs, nzLightboxCtrl) {
+					element.on('click', function() {
+						nzLightboxCtrl.acceptForm();
+					})
+					$compile(element, null, 500)(scope);
+				}
+			}
+		}
+	}]);
+
+	module.directive('lightboxRejectAction', ['$compile', function($compile) {
+		return {
+			priority: 500,
+			terminal: true,
+			require: '^nzLightboxLogic',
+			compile: function($element, $attrs) {
+				return function(scope, element, attrs, nzLightboxCtrl) {
+					element.on('click', function() {
+						nzLightboxCtrl.rejectForm();
+					})
+					$compile(element, null, 500)(scope);
+				}
+			}
+		}
+	}]);
+
+})(angular);
--- example.js
+++ example.js
@@ -3,21 +3,34 @@
 
 	var module = angular.module('net.enzey.example',
 		[
-			'net.enzey.dialog'
+			'net.enzey.lightbox'
 		]
 	);
 
 	module.controller('dialogManagerCtrl', function ($scope) {
-		$scope.itemList = [
-			{text: "red"},
-			{text: "blue"},
-			{text: "purple"},
-			{text: "while"},
-			{text: "black"},
-		];
-		$scope.show = function() {
-			$scope.$broadcast('displayDialog');
+		$scope.show = function(event) {
+			$scope.$broadcast(event);
 		}
+		$scope.fucks = [
+			{id: '', label: ''},
+			{id: '3241', label: 'dsafg'},
+			{id: '54674', label: 'fghj'},
+		]
 	});
 
+	module.directive('dropdownSelectionRequired', function() {
+		return {
+			restrict: 'A', // only activate on element attribute
+			require: '?ngModel', // get a hold of NgModelController
+			link: function(scope, element, attrs, ngModel) {
+				ngModel.$validators.hasSelection = function(modelValue, viewValue) {
+					return modelValue ? true : false;
+				}
+				scope.fucks = [
+					{id: '3241', label: 'dsafg'},
+					{id: '54674', label: 'fghj'},
+				]
+			}
+		}
+	});
 })(angular);
